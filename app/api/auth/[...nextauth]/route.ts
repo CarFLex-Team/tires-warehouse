2ad2103bcaa203764e15832307c7e9b1f0ff9cf2
Mazen Essam <mazenessam2011@outlook.com>
2026-01-04 21:50:16 +0200

    Finish Authentication

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,12 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import type { JWT } from "next-auth/jwt";
 import type { Session } from "next-auth";
-
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 declare module "next-auth" {
   interface Session {
     user: {
+      id: number;
       email?: string | null;
       name?: string | null;
       role?: string;
@@ -15,10 +17,15 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
   interface JWT {
+    id: number;
     role?: string;
   }
 }
+
 const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -27,31 +34,26 @@ const handler = NextAuth({
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        // 🔴 MOCK USERS (replace later with DB)
-        const users = [
-          {
-            id: "1",
-            name: "Admin User",
-            email: "admin@gmail.com",
-            role: "admin",
-            password: "admin123",
-          },
-          {
-            id: "2",
-            name: "Cashier User",
-            email: "cashier@example.com",
-            role: "team-member",
-            password: "cashier123",
-          },
-        ];
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
 
-        const user = users.find(
-          (u) =>
-            u.email === credentials?.email &&
-            u.password === credentials?.password
-        );
+        const result = await db
+          .query(
+            ' SELECT id, email, password, role FROM "User" WHERE email = $1',
+            [credentials.email]
+          )
+          .then((res) => res)
+          .catch((err) => {
+            console.error("Database query error:", err);
+            return { rows: [] };
+          });
 
-        if (!user) return null;
+        const user = result.rows[0];
+
+        const valid = await bcrypt.compare(credentials.password, user.password);
+
+        if (!valid) return null;
 
         return {
           id: user.id,
@@ -63,18 +65,23 @@ const handler = NextAuth({
     }),
   ],
   callbacks: {
-    jwt({ token, user }: { token: JWT; user?: any }) {
+    async jwt({ token, user }: { token: JWT; user?: any }) {
       if (user) {
+        token.id = user.id;
         token.role = user.role;
       }
       return token;
     },
     session({ session, token }: { session: Session; token: JWT }) {
       if (session.user) {
+        session.user.id = token.id as number;
         session.user.role = token.role;
       }
       return session;
     },
+  },
+  pages: {
+    signIn: "/login",
   },
 });
 
