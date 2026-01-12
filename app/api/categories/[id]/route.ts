a6f0bc75cb3065bc333fff930b25ffc79a9f73a1
Mazen Essam <mazenessam2011@outlook.com>
2026-01-12 21:48:06 +0200

    edit delete id context

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -3,10 +3,10 @@ import { db } from "@/lib/db";
 
 export async function DELETE(
   _req: Request,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = await params;
+    const { id } = await context.params;
     console.log("Deleting category with id:", id);
     await db.query(
       `
